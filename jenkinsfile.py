1ba512719de590d6b207814c3b1abcd660e90cdd
RishonaLancy <rishona.lancy2024@vitstudent.ac.in>
2026-09-17 15:17:07 +0530

    Update jenkinsfile.py

--- jenkinsfile.py
+++ jenkinsfile.py
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/<student-username>/<repo-name>.git'
+                git branch: 'main', url: 'https://github.com/RishonaLancy/project3.git'
             }
         }
 
